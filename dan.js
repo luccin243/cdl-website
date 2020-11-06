afb22094bc8bf0940a8031a7293cd40c74fa852a
merdi kim <merdikimuha1@gmail.com>
2020-11-06 10:22:58 -0800

    dan hbd

---
+++ dan.js
@@ -0,0 +1,5 @@
+
+function getPic() {
+    let title = document.querySelector("#pic");
+    title.src="https://www.birthdaywishes.expert/wp-content/uploads/2015/11/happy-birthday-messages-500x500.jpg";
+}
